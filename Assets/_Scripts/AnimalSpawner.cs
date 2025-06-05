7792ed0da70180f233a084e1aa8d390de08dcce0
Wiktor Ziędalski <wiktor.karol@outlook.com>
2025-06-05 13:09:03 +0200

    all tiers

--- Assets/_Scripts/AnimalSpawner.cs
+++ Assets/_Scripts/AnimalSpawner.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AnimalSpawner : MonoBehaviour
@@ -6,10 +7,11 @@ public class AnimalSpawner : MonoBehaviour
     [SerializeField] private GameObject animalPrefab;
     [SerializeField] private AnimalType[] animalTypes;
     [SerializeField] private CircleCollision circleCollision;
+    [SerializeField] private Transform spawnPosition;
 
     void Start()
     {
-        //StartCoroutine(autoSpawn());
+        StartCoroutine(autoSpawn());
     }
 
     void Update()
@@ -17,8 +19,13 @@ public class AnimalSpawner : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            SpawnAnimal(mousePosition, 0);
+            SpawnNewAnimal(mousePosition, 0);
         }
+    }
+
+    private void SpawnNewAnimal(Vector2 position, int tier)
+    {
+        SpawnAnimal(new Vector2(position.x, spawnPosition.position.y), tier);
     }
 
     public void SpawnAnimal(Vector2 position, int tier)
@@ -28,23 +35,29 @@ public class AnimalSpawner : MonoBehaviour
             Debug.LogWarning("Invalid tier specified for animal spawning.");
             return;
         }
-        GameObject animal = Instantiate(animalPrefab, position, Quaternion.identity);
+        Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
+        GameObject animal = Instantiate(animalPrefab, position, randomRotation);
         animal.GetComponent<AnimalData>().SetAnimalType(animalTypes[tier]);
         CircleCollider2D collider = animal.GetComponent<CircleCollider2D>();
         collider.radius = animalTypes[tier].colliderRadius;
         collider.offset = new Vector2(animalTypes[tier].colliderOffsetX, animalTypes[tier].colliderOffsetY);
         animal.GetComponent<SpriteRenderer>().sprite = animalTypes[tier].animalSprite;
         animal.transform.localScale = new Vector3(animalTypes[tier].localScale, animalTypes[tier].localScale, 1f);
-        circleCollision.AddAnimal(animal.transform, animal.GetComponent<AnimalData>());
+        circleCollision.AddAnimal(animal.transform, animal.GetComponent<AnimalData>(), tier);
+    }
+
+    public int GetAnimalCount()
+    {
+        return animalTypes.Length;
     }
 
     IEnumerator autoSpawn()
     {
         while (true)
         {
-            yield return new WaitForSeconds(.05f);
+            yield return new WaitForSeconds(.1f);
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            SpawnAnimal(mousePosition, 0);
+            SpawnNewAnimal(mousePosition, 0);
         }
         
     }
--- Assets/_Scripts/CircleCollision.cs
+++ Assets/_Scripts/CircleCollision.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 
 public class CircleCollision : MonoBehaviour
 {
-    [SerializeField] private List<Transform> animalObjects = new List<Transform>();
-    [SerializeField] private List<AnimalData> animalDatas = new List<AnimalData>();
+    [SerializeField] private List<List<Pair<Transform, AnimalData>>> animalObjectsAndDatas = new List<List<Pair<Transform, AnimalData>>>();
+    //[SerializeField] private List<List<AnimalData>> animalDatas = new List<AnimalData>();
     [SerializeField] private AnimalSpawner animalSpawner;
     void Start()
     {
-        
+        for (int i = 0; i < animalSpawner.GetAnimalCount(); i++)
+        {
+            animalObjectsAndDatas.Add(new List<Pair<Transform, AnimalData>>());
+        }
     }
 
     void Update()
@@ -18,20 +21,21 @@ public class CircleCollision : MonoBehaviour
 
     private void CheckCollisions()
     {
-        for (int i = 0; i < animalObjects.Count; i++)
+        for (int i = 0; i < animalObjectsAndDatas.Count; i++ /*every animal tier*/)
         {
-            for (int j = i + 1; j < animalObjects.Count; j++)
+            for (int j = 0; j < animalObjectsAndDatas[i].Count; j++ /*every animal in tier*/)
             {
-                if (animalObjects[i] == null || animalObjects[j] == null)
-                    continue;
-
-                if (animalDatas[i].GetAnimalType() != animalDatas[j].GetAnimalType())
-                    continue;
-                
-                float distance = Vector2.Distance(animalObjects[i].position, animalObjects[j].position);
-                if (distance <= animalDatas[i].GetAnimalType().colliderRadius * animalDatas[i].GetAnimalType().localScale * 2 + 0.01f)
+                for (int k = j + 1; k < animalObjectsAndDatas[i].Count; k++ /*every animal in tier*/)
                 {
-                    HandleCollision(animalDatas[i], animalDatas[j], animalDatas[i].GetAnimalType().tier);
+                    if (animalObjectsAndDatas[i][j] == null || animalObjectsAndDatas[i][k] == null)
+                        continue;
+                    if (animalObjectsAndDatas[i][j].First == null || animalObjectsAndDatas[i][k].First == null)
+                        continue;
+                    float distance = Vector2.Distance(animalObjectsAndDatas[i][j].First.position, animalObjectsAndDatas[i][k].First.position);
+                    if (distance <= animalObjectsAndDatas[i][j].Second.GetAnimalType().colliderRadius * animalObjectsAndDatas[i][j].Second.GetAnimalType().localScale * 2 + 0.1f)
+                    {
+                        HandleCollision(animalObjectsAndDatas[i][j].Second, animalObjectsAndDatas[i][k].Second, i);
+                    }
                 }
             }
         }
@@ -39,17 +43,20 @@ public class CircleCollision : MonoBehaviour
 
     private void HandleCollision(AnimalData animal1, AnimalData animal2, int tier)
     {
-        animalObjects.Remove(animal1.transform);
-        animalObjects.Remove(animal2.transform);
-        animalDatas.Remove(animal1);
-        animalDatas.Remove(animal2);
+        if (tier == animalSpawner.GetAnimalCount() - 1) return;
+        animalObjectsAndDatas[animal1.GetAnimalType().tier].Remove(new Pair<Transform, AnimalData>(animal1.transform, animal1));
+        animalObjectsAndDatas[animal2.GetAnimalType().tier].Remove(new Pair<Transform, AnimalData>(animal2.transform, animal2));
+        //animalObjects.Remove(animal1.transform);
+        //animalObjects.Remove(animal2.transform);
+        //animalDatas.Remove(animal1);
+        //animalDatas.Remove(animal2);
         Destroy(animal1.gameObject);
         Destroy(animal2.gameObject);
         animalSpawner.SpawnAnimal((animal1.transform.position + animal2.transform.position)/2, tier + 1);
         Debug.Log($"Collision detected between {animal1.name} and {animal2.name}");
     }
 
-    public void AddAnimal(Transform animalTransform, AnimalData animalData)
+    public void AddAnimal(Transform animalTransform, AnimalData animalData, int tier)
     {
         if (animalTransform == null || animalData == null)
         {
@@ -57,14 +64,23 @@ public class CircleCollision : MonoBehaviour
             return;
         }
 
-        if (animalDatas.Count != animalObjects.Count)
-        {
-            Debug.LogError("Fuckin nie rowna ilosc animal datas i animal objects");
-            return;
-        }
-
-        animalObjects.Add(animalTransform);
-        animalDatas.Add(animalData);
+        animalObjectsAndDatas[tier].Add(new Pair<Transform, AnimalData>(animalTransform, animalData));
         animalTransform.SetParent(transform);
     }
 }
+
+public class Pair<T, U>
+{
+    public Pair()
+    {
+    }
+
+    public Pair(T first, U second)
+    {
+        this.First = first;
+        this.Second = second;
+    }
+
+    public T First { get; set; }
+    public U Second { get; set; }
+};
